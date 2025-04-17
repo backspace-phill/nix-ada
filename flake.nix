63c6e0d5f9c9e109e97bd7e7275e7eff2189da37
Phillip <psusemiehl@gmail.com>
2025-04-17 19:30:39 +0200

    made flake multisystem for maybe darwin...

--- flake.nix
+++ flake.nix
@@ -1,17 +1,31 @@
 {
-   #inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-   inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=310c5c4d3fd457d80365f52c71ac80b9d54a7cc4";
+  #inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=310c5c4d3fd457d80365f52c71ac80b9d54a7cc4";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-   outputs = { self, nixpkgs }:
-   let
-      system = "x86_64-linux";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        # system = "x86_64-linux";
+        # system = builtins.currentSystem;
 
-      pkgs = import nixpkgs { inherit system; overlays = (import ./overlays.nix {}); };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = (import ./overlays.nix { });
+        };
 
-      nix-ada = import ./default.nix { inherit pkgs; };
-   in {
-      packages.${system} = nix-ada;
+        nix-ada = import ./default.nix { inherit pkgs; };
+      in
+      {
+        packages.${system} = nix-ada;
 
-      devShells.${system}.default = import ./shell.nix { inherit nix-ada; };
-   };
+        devShells.default = import ./shell.nix { inherit nix-ada; };
+      }
+    );
 }
